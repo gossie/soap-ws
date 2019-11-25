59d07eb43cafa63f22a3a1171d1554c4169a5911
André Schreck <andre.schreck@gmail.com>
2019-11-25 14:26:25 +0100

    index.html

---
+++ src/main/java/com/example/demo/IndexController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping(path = "/")
+    public String root() {
+        return index();
+    }
+
+    @GetMapping(path = "/index.html")
+    public String index() {
+        return "index";
+    }
+
+
+}
